0e136a18a20a0c7084deef5df27b3bf0d50723c0
Marc Jacquart <marc.jacquart@epfl.ch>
2021-03-01 16:50:36 +0100

    Add first version of Example.py, import the data and variables

---
+++ Example.py
@@ -0,0 +1,74 @@
+#Import libraries:
+from root_pandas import read_root
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Define useful variables, split into categories to match root variables names (see line 15):
+common = [  'P',                    # Momentum
+            'PT',                   # Transverse momentum
+            'ETA',                  # Pseudorapidity
+            'IP_OWNPV',             # Impact parameter "OWNPV": primary vertex reconstructed with the tracks of other products of pp collision
+            'IPCHI2_OWNPV',]        # How to apply???CHi2: Staticical test to see if a variable follows a given probability function
+
+Bvars  = [  'DTF_M',                # Reconstructed invarient mass
+            'DOCA',                 # Distance of closest approach, high for background
+            'ENDVERTEX_CHI2',       # ---
+            'BPVDIRA',              # Computes the cosine of the angle between the momentum of the particle and the direction fo flight from the best PV to the decay vertex.
+            'TAU',                  # Proper time before decay
+            'FD_OWNPV',             # Flight distance from primary vertex
+            'CDFiso',               # Isolation (see if other track in a cone arount the selected track. If so probably background)
+            'D1_isolation_Giampi',  # ---
+            'D2_isolation_Giampi',] # ---
+
+muvars = [  'ProbNNmu',             # Probability neural network mu?
+            'ProbNNk',]             # --- same for kaon?
+
+evars  = [  'ProbNNe',]             # --- same for electron?
+
+
+#Fill tab with variable names:
+allvars=[]
+for var in Bvars:
+    allvars.append(f'B_s0_{var}')
+for var in muvars:
+    allvars.append(f'muminus_{var}')
+for var in evars:
+    allvars.append(f'eplus_{var}')
+for var in common:
+    for p in ('B_s0', 'eplus', 'muminus'):
+        allvars.append(f'{p}_{var}')
+        
+allvars.append('eventNumber')
+
+#Display chosen variables:
+print(allvars) 
+
+# Data path & import with variable names:
+clusterPath="/panfs/tully/B2emu"
+X_data_2016_MU = read_root(clusterPath+"/data/Selected_B2emu2016MU.root", columns=allvars)
+X_data_2016_MD = read_root(clusterPath+"/data/Selected_B2emu2016MD.root", columns=allvars)
+X_MC_2016_MU = read_root(clusterPath+"/MC/Selected_B2emu2016MU.root", columns=allvars)
+X_MC_2016_MD = read_root(clusterPath+"/MC/Selected_B2emu2016MD.root", columns=allvars)
+
+#Putting togeter up and down polarity for larger data sample:
+X_data_2016 = pd.concat([X_data_2016_MU, X_data_2016_MD]) 
+X_MC_2016 = pd.concat([X_MC_2016_MU, X_MC_2016_MD])
+
+#Train above the target to get only noise:
+X_MC_2016 = X_MC_2016.loc[(X_MC_2016['B_s0_DTF_M'] > 5500) & (X_MC_2016['B_s0_DTF_M'] < 6500)]
+X_data_2016 = X_data_2016.loc[(X_data_2016['B_s0_DTF_M'] > 5500) & (X_data_2016['B_s0_DTF_M'] < 6500)] 
+
+
+for var in allvars:
+    range_low  = min(X_data_2016[var].min(), X_MC_2016[var].min())
+    range_high = max(X_data_2016[var].max(), X_MC_2016[var].max())
+
+    plt.hist(X_data_2016[var], bins=100, label='data', density=True, alpha=0.7, range=(range_low, range_high))
+    plt.hist(X_MC_2016[var],   bins=100, label='MC',   density=True, alpha=0.7, range=(range_low, range_high))
+
+    plt.xlabel(var)
+    plt.ylabel('Arbitrary units')
+    
+    plt.legend(frameon=False)
+    plt.savefig(f'plots/data_MC_{var}.pdf')
+    plt.close()
