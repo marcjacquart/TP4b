cdc85c09c82a19882476d2d8b89d8076c5c79898
Marc Jacquart <marc.jacquart@epfl.ch>
2021-03-06 16:13:13 +0100

    Adding variable descriptions

--- Example.py
+++ Example.py
@@ -4,23 +4,23 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 #Define useful variables, split into categories to match root variables names (see line 15):
-common = [  'P',                    # Momentum
-            'PT',                   # Transverse momentum
+common = [  'P',                    # Momentum (not used)
+            'PT',                   # Transverse momentum, We will use only B_(P_T)
             'ETA',                  # Pseudorapidity
-            'IP_OWNPV',             # Impact parameter "OWNPV": primary vertex reconstructed with the tracks of other products of pp collision
-            'IPCHI2_OWNPV',]        # How to apply???CHi2: Staticical test to see if a variable follows a given probability function
+            'IP_OWNPV',             # Impact parameter with respect to his own production primary vertex "OWNPV": primary vertex reconstructed with the tracks of other products of collision
+            'IPCHI2_OWNPV',]        # Chi2 of the PV fit: how well is the PV defined
 
 Bvars  = [  'DTF_M',                # Reconstructed invarient mass
-            'DOCA',                 # Distance of closest approach, high for background
-            'ENDVERTEX_CHI2',       # ---
+            'DOCA',                 # Distance of closest approach, high for background. Meaning for a decay???
+            'ENDVERTEX_CHI2',       # Quality of the reconstructed decay vertex.
             'BPVDIRA',              # Computes the cosine of the angle between the momentum of the particle and the direction fo flight from the best PV to the decay vertex.
             'TAU',                  # Proper time before decay
             'FD_OWNPV',             # Flight distance from primary vertex
             'CDFiso',               # Isolation (see if other track in a cone arount the selected track. If so probably background)
-            'D1_isolation_Giampi',  # ---
-            'D2_isolation_Giampi',] # ---
+            'D1_isolation_Giampi',  # Giampi's isolation tool: number of tracks in a cone of angle 0.27 rad around the reconstructed track.
+            'D2_isolation_Giampi',] # We want the sum of both isolation number. signal will be 0 (no other close trcks in the daughter particles)
 
-muvars = [  'ProbNNmu',             # Probability neural network mu?
+muvars = [  'ProbNNmu',             # Bayesian posteriori probability: The posterior probability distribution is the probability distribution of an unknown quantity, treated as a random variable, conditional on the evidence obtained from an experiment or survey. 
             'ProbNNk',]             # --- same for kaon?
 
 evars  = [  'ProbNNe',]             # --- same for electron?
