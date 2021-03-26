678aa0de099f56fa497695f5bea104000813c109
Marc Jacquart <marc.jacquart@epfl.ch>
2021-03-26 22:36:12 +0100

    Improve comments of FeatureSelection.py

--- FeatureSelection.py
+++ FeatureSelection.py
@@ -1,5 +1,8 @@
 # This script:
 #	-Tries to reduce the number of features for he BDT by recursive feature elimination
+#	-Plot the roc curve and compute auc to measure model performence with given number of features
+#	-Accept variable number of selected parameters at script call for scan
+#	-Print result in csv file for performance analysis with respect to number of features 
 
 
 from sklearn.tree import DecisionTreeClassifier
@@ -18,50 +21,37 @@ from sys import argv									# To pass variables
 
 
 
-pathCSV='/home/mjacquar/TP4b/csv'
+pathCSV='/home/mjacquar/TP4b/csv'						# Import Data
 X = pd.read_csv(f'{pathCSV}/X.csv')
 y = X['sig']
 print("csv loaded")
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.7, train_size=0.3, random_state=0) #random state: seed for random assignation of data in the split, done wih kFold
 
-# From the selection in Example.py
+# From the selection in Example.py, 24 features selected
 features = ['B_s0_TAU', 'B_s0_DTF_M', 'B_s0_ENDVERTEX_CHI2', 'B_s0_BPVDIRA', 'B_s0_CDFiso', 'B_s0_D1_isolation_Giampi', 'B_s0_D2_isolation_Giampi', 'muminus_ProbNNmu', 'muminus_ProbNNk', 'eplus_ProbNNe', 'muminus_PT', 'eplus_ETA', 'muminus_ETA', 'B_s0_IPCHI2_OWNPV', 'eplus_IPCHI2_OWNPV', 'B_s0_minP', 'B_s0_absdiffP', 'B_s0_minPT', 'B_s0_minIP_OWNPV', 'B_s0_absdiffIPCHI2_OWNPV', 'MIN_IPCHI2_emu', 'LOG1_cosDIRA', 'MAX_PT_emu', 'DIFF_ETA_emu']
-#nFeaturesTot=len(features)
-#print (nFeaturesTot)
-nSelect = int(argv[1]) 									# For the scan. Need to convert the string to int
-#print(nSelect)
+
+nSelect = int(argv[1]) 														# For the scan. Need to convert the string to int
 dt    = DecisionTreeClassifier(max_depth=3)
 model = AdaBoostClassifier(dt, algorithm='SAMME.R', n_estimators=50, learning_rate=0.1)
 
 sel = RFE(estimator=model,n_features_to_select=nSelect,step=1, verbose=0) 	# Recursive feature elimination. step=1: eject one feature at each step
-
 sel.fit(X_train[features], y_train)											# Train the model
-
 
 pathModel='/home/mjacquar/TP4b/modelOpti'									# https://medium.com/@harsz89/persist-reuse-trained-machine-learning-models-using-joblib-or-pickle-in-python-76f7e4fd707
 joblib.dump(sel,f'{pathModel}/selection_3_50_0.1_{nSelect}variables.pkl')	# Save trained model for later
 
-#print(sel.get_support())
+# Extract selected feature list:
 featureArray=np.array(features)
-#print(featureArray[sel.get_support()])					# Support is boolean array with selected index=True
-finalFeatures=featureArray[sel.get_support()]
+finalFeatures=featureArray[sel.get_support()] 								# Support is boolean array with selected index=True
 
 # Compute roc curve to measure performances:
-y_pred=sel.predict_proba(X_test[features])[:,1]			# 2 values, takes first: proba to be signal
-fpr, tpr, threshold = roc_curve(y_test, y_pred) 		# Use built in fct to compute:  false/true positive read, using the answer and predictions of the test sample
-aucValue = auc(fpr, tpr) 								# Use built in fct to compute area under curve
-print(f'Auc value: {aucValue}')
+y_pred=sel.predict_proba(X_test[features])[:,1]								# 2 values, takes first: proba to be signal
+fpr, tpr, threshold = roc_curve(y_test, y_pred) 							# Use built in fct to compute:  false/true positive read, using the answer and predictions of the test sample
+aucValue = auc(fpr, tpr) 													# Use built in fct to compute area under curve
 
-
-
+# Save results in csv file for later analysis:
 pathCSV='/home/mjacquar/TP4b/csv'
 with open(f'{pathCSV}/paramSelection.csv', 'a', newline='') as csvfile:		# 'a': append mode to not overwrite
 	spamwriter = csv.writer(csvfile, delimiter=' ')
 	spamwriter.writerow([nSelect,aucValue,finalFeatures])
-
-#selectedFeatures = features[sel.get_support()] # support is a boolean array. true = feature is selected
-#print (selectedFeatures)
-
-# Tuning hyperparameters: https://scikit-learn.org/stable/modules/grid_search.html#grid-search
-# Feature selection: https://scikit-learn.org/stable/modules/feature_selection.html
