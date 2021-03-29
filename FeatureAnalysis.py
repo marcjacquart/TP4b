f8cf5cf2a37e96196192057124ca615da1893e6b
Marc Jacquart <marc.jacquart@epfl.ch>
2021-03-29 19:19:07 +0200

    Adding RFE scan with updated variables

--- FeatureAnalysis.py
+++ FeatureAnalysis.py
@@ -5,13 +5,19 @@ import numpy as np
 import csv
 
 pathCSV='/home/mjacquar/TP4b/csv'
-df = pd.read_csv(f'{pathCSV}/paramSelection.csv',sep=' ',names=['nFeatures','Auc',''])
+dfPID = pd.read_csv(f'{pathCSV}/paramSelectionWithPID.csv',sep=' ',names=['nFeatures','Auc','featureList'])
+df = pd.read_csv(f'{pathCSV}/paramSelectionWithoutPID.csv',sep=' ',names=['nFeatures','Auc','featureList'])
+dfIso = pd.read_csv(f'{pathCSV}/paramSelectionWithoutB0Mass.csv',sep=' ',names=['nFeatures','Auc','featureList'])
 #print(df)
 plt.figure(figsize=(8,6), dpi=500)
 fig, ax = plt.subplots() 
-plt.ylim(0.9995,0.99965)
-ax.plot(df['nFeatures'],df['Auc'],'k.')
+#plt.ylim(0.9893,0.9897)
+plt.ylim(0.989,0.99)
+ax.plot(dfPID['nFeatures'],dfPID['Auc'],'r.',label='With PID features')
+ax.plot(df['nFeatures'],df['Auc'],'b.',label='Without PID features')
+ax.plot(dfIso['nFeatures'],dfIso['Auc'],'g.',label='With PID features and 2 separate isolation variables')
 #ax.set_yscale('log')
+plt.legend()
 plt.xlabel("Number of features")
 plt.ylabel("Auc")
 plt.savefig(f'plots/featuresAuc.pdf')
--- FeatureSelection.py
+++ FeatureSelection.py
@@ -24,13 +24,14 @@ from sys import argv									# To pass variables
 pathCSV='/home/mjacquar/TP4b/csv'						# Import Data
 X = pd.read_csv(f'{pathCSV}/X.csv')
 y = X['sig']
-print("csv loaded")
+#print("csv loaded")
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.7, train_size=0.3, random_state=0) #random state: seed for random assignation of data in the split, done wih kFold
 
 # From the selection in Example.py, 24 features selected
-features = ['B_s0_TAU', 'B_s0_DTF_M', 'B_s0_ENDVERTEX_CHI2', 'B_s0_BPVDIRA', 'B_s0_CDFiso', 'B_s0_D1_isolation_Giampi', 'B_s0_D2_isolation_Giampi', 'muminus_ProbNNmu', 'muminus_ProbNNk', 'eplus_ProbNNe', 'muminus_PT', 'eplus_ETA', 'muminus_ETA', 'B_s0_IPCHI2_OWNPV', 'eplus_IPCHI2_OWNPV', 'B_s0_minP', 'B_s0_absdiffP', 'B_s0_minPT', 'B_s0_minIP_OWNPV', 'B_s0_absdiffIPCHI2_OWNPV', 'MIN_IPCHI2_emu', 'LOG1_cosDIRA', 'MAX_PT_emu', 'DIFF_ETA_emu']
-
+features = ['B_s0_TAU', 'B_s0_ENDVERTEX_CHI2', 'B_s0_BPVDIRA', 'B_s0_CDFiso', 'B_s0_D1_isolation_Giampi', 'B_s0_D2_isolation_Giampi', 'muminus_ProbNNmu', 'muminus_ProbNNk', 'eplus_ProbNNe', 'eplus_PT', 'muminus_PT', 'eplus_ETA', 'muminus_ETA', 'eplus_IP_OWNPV', 'B_s0_IPCHI2_OWNPV', 'B_s0_minP', 'B_s0_absdiffP', 'B_s0_minPT', 'B_s0_absdiffPT', 'B_s0_absdiffETA', 'B_s0_minIP_OWNPV', 'B_s0_absdiffIP_OWNPV', 'MIN_IPCHI2_emu', 'LOG1_cosDIRA']
+# with ProbNN: ['B_s0_TAU', 'B_s0_ENDVERTEX_CHI2', 'B_s0_BPVDIRA', 'B_s0_CDFiso', 'muminus_ProbNNmu', 'muminus_ProbNNk', 'eplus_ProbNNe', 'eplus_PT', 'muminus_PT', 'eplus_ETA', 'muminus_ETA', 'eplus_IP_OWNPV', 'B_s0_IPCHI2_OWNPV', 'B_s0_minP', 'B_s0_absdiffP', 'B_s0_minPT', 'B_s0_absdiffPT', 'B_s0_absdiffETA', 'B_s0_minIP_OWNPV', 'B_s0_absdiffIP_OWNPV', 'MIN_IPCHI2_emu', 'SUM_isolation_emu', 'LOG1_cosDIRA']
+# without ProbNN: ['B_s0_TAU', 'B_s0_ENDVERTEX_CHI2', 'B_s0_BPVDIRA', 'B_s0_CDFiso', 'eplus_PT', 'muminus_PT', 'eplus_ETA', 'muminus_ETA', 'eplus_IP_OWNPV', 'B_s0_IPCHI2_OWNPV', 'B_s0_minP', 'B_s0_absdiffP', 'B_s0_minPT', 'B_s0_absdiffPT', 'B_s0_absdiffETA', 'B_s0_minIP_OWNPV', 'B_s0_absdiffIP_OWNPV', 'MIN_IPCHI2_emu', 'SUM_isolation_emu', 'LOG1_cosDIRA']
 nSelect = int(argv[1]) 														# For the scan. Need to convert the string to int
 dt    = DecisionTreeClassifier(max_depth=3)
 model = AdaBoostClassifier(dt, algorithm='SAMME.R', n_estimators=50, learning_rate=0.1)
@@ -38,7 +39,7 @@ model = AdaBoostClassifier(dt, algorithm='SAMME.R', n_estimators=50, learning_ra
 sel = RFE(estimator=model,n_features_to_select=nSelect,step=1, verbose=0) 	# Recursive feature elimination. step=1: eject one feature at each step
 sel.fit(X_train[features], y_train)											# Train the model
 
-pathModel='/home/mjacquar/TP4b/modelOpti'									# https://medium.com/@harsz89/persist-reuse-trained-machine-learning-models-using-joblib-or-pickle-in-python-76f7e4fd707
+pathModel='/home/mjacquar/TP4b/modelOpti/featureSelectionWithoutB0Mass'		# https://medium.com/@harsz89/persist-reuse-trained-machine-learning-models-using-joblib-or-pickle-in-python-76f7e4fd707
 joblib.dump(sel,f'{pathModel}/selection_3_50_0.1_{nSelect}variables.pkl')	# Save trained model for later
 
 # Extract selected feature list:
@@ -52,6 +53,6 @@ aucValue = auc(fpr, tpr) 													# Use built in fct to compute area under c
 
 # Save results in csv file for later analysis:
 pathCSV='/home/mjacquar/TP4b/csv'
-with open(f'{pathCSV}/paramSelection.csv', 'a', newline='') as csvfile:		# 'a': append mode to not overwrite
+with open(f'{pathCSV}/paramSelectionWithoutB0Mass.csv', 'a', newline='') as csvfile:		# 'a': append mode to not overwrite
 	spamwriter = csv.writer(csvfile, delimiter=' ')
 	spamwriter.writerow([nSelect,aucValue,finalFeatures])
