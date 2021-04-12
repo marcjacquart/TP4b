312d1ede595b986ad5b1cbc86dc5dc1ef6d4ce33
Marc Jacquart <marc.jacquart@epfl.ch>
2021-04-12 16:32:22 +0200

    plot 2d and 3d distance between response peaks

--- Analyse.py
+++ Analyse.py
@@ -14,20 +14,24 @@ import joblib
 from sklearn.metrics import roc_curve, auc 	# roc: receiver operating characteristic, auc: Area under the ROC Curve
 from sklearn.model_selection import train_test_split
 from scipy.stats import ks_2samp 			# Kolmogorov–Smirnov test to compare 2 distributions
+from Functions import predictionY_interval
 #from scipy import stats
+
+
+
+
 
 plotRoc=True
 plotPrediction_KS=True
 modelType='hyperScan' 		# 'featureSelection' or 'hyperScan' /!\ also change between the two sets of features
 plotImportance=True
 
-
 # Import data:
 pathCSV='/home/mjacquar/TP4b/csv'
 X = pd.read_csv(f'{pathCSV}/X.csv')
 y = X['sig']						# 1: signal, 0: background
 print("csv loaded")
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, train_size=0.7, random_state=0) # est_size=0.3, train_size=0.7. random state: seed for random assignation of data in the split. Seed given so the test sample is different of training sample even after saving and reopening the bdt
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, train_size=0.7, random_state=0) # test_size=0.3, train_size=0.7. random state: seed for random assignation of data in the split. Seed given so the test sample is different of training sample even after saving and reopening the bdt
 
 # features = ['B_s0_TAU',				# B lifetime
 # 			'MIN_IPCHI2_emu',		# Minimum on the two impact parameters chi2, large if signal (comes from secondary vertex)
@@ -39,7 +43,16 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, train_s
 # 			'MAX_PT_emu',			# B0 has high mass -> high p_T for daughter particles 
 # 			'B_s0_ENDVERTEX_CHI2',	# Quality of the reconstructed decay vertex.
 # 			'DIFF_ETA_emu']			# Absolute difference in pseudorapidity of emu
-features = ['B_s0_ENDVERTEX_CHI2', 'B_s0_CDFiso', 'muminus_ProbNNmu', 'eplus_ProbNNe', 'eplus_ETA', 'B_s0_IPCHI2_OWNPV', 'B_s0_minPT', 'MIN_IPCHI2_emu','SUM_isolation_emu', 'LOG1_cosDIRA'] 
+features = ['B_s0_ENDVERTEX_CHI2', 
+			'B_s0_CDFiso', 
+			'muminus_ProbNNmu', 
+			'eplus_ProbNNe', 
+			'eplus_ETA', 
+			'B_s0_IPCHI2_OWNPV',
+			 'B_s0_minPT', 
+			 'MIN_IPCHI2_emu',
+			 'SUM_isolation_emu', 
+			 'LOG1_cosDIRA'] 
 
 #  Retrive model: Select with name from parameters, best .R: 5,130,0.225,// .R first: 6,100,0.15 // best SAMME: 10,1000.1
 pathModel='/home/mjacquar/TP4b/modelOpti/HyperScanFromRFE' # Change name of folder for different scans
@@ -89,46 +102,6 @@ if plotRoc:
 	plt.legend()
 	plt.savefig(f'plots/roc.pdf')
 	plt.close()
-
-#				Known result 	 Predicted results			  Want to plot?	 For Multianalysis 	for pdf save
-def predictionY_interval(y_train, y_test, y_pred_onTrain,y_pred_onTest, plotResult,   computeInterval,    pdfName='yPred'): #Function form to use in Multianalysis
-	yTrainS=y_pred_onTrain[y_train==1]		# Take values at index depending of BDT y output
-	yTrainB=y_pred_onTrain[y_train==0]
-	yTestS=y_pred_onTest[y_test==1]
-	yTestB=y_pred_onTest[y_test==0]
-
-	binsTab=np.linspace(0, 1, num=201) 	# num: number of bins for smooth histogram.
-	if plotResult:
-		plt.figure(figsize=(8, 8), dpi=300)
-		fig, ax = plt.subplots() 																	# Want to stack histograms on top of same axes
-		ax.hist(yTrainS,  bins=binsTab, color='r',histtype='bar',density=True, alpha=0.3) 			# Adding transparency for histogram columns
-		ax.hist(yTrainB,  bins=binsTab, color='b', histtype='bar',density=True, alpha=0.3) 			# collect the results in varibles for later KS test
-		yTrainSHist = ax.hist(yTrainS,  bins=binsTab, color='r', histtype='step',density=True, label='Sig (train)') 	# Histogram values
-		yTrainBHist = ax.hist(yTrainB,  bins=binsTab, color='b', histtype='step',density=True, label='Bg (train)') 
-		yTestSHist = plt.hist(yTestS,   bins=binsTab, density=True,alpha = 0.0)						# Invisible to collect histogram values
-		yTestBHist = plt.hist(yTestB,   bins=binsTab, density=True,alpha = 0.0)
-		bin_centers = 0.5*(binsTab[1:] + binsTab[:-1])
-		ax.scatter(bin_centers, yTestSHist[0], marker='o', c='r', s=20, alpha=1,label='Sig (test)') 	# Display as dot in histogram
-		ax.scatter(bin_centers, yTestBHist[0], marker='o', c='b', s=20, alpha=1,label='Bg (test)') 	# Must take first array for data, second one is bins values
-
-		plt.xlabel('BDT signal response')
-		plt.ylabel('Normalized number of events')
-		plt.legend()
-		plt.savefig(f'plots/{pdfName}.pdf')
-		plt.close()	
-	if computeInterval:
-		numberBins=201
-		errorMax=2*(1/(numberBins-1))
-		binsTab=np.linspace(0, 1, num=numberBins)
-		
-		yTestSHist=np.histogram(yTestS,bins=binsTab,density=False) #Density=True to normalise, not necessary here we just want the max
-		yTestBHist=np.histogram(yTestB,bins=binsTab,density=False)
-		maxS=np.argmax(yTestSHist[0]) #first tab is the histogram values, second is the bins values on axis
-		maxB=np.argmax(yTestBHist[0])
-		interval=abs(binsTab[maxS]-binsTab[maxB])
-		print(f'Interval between Signal and backgroung max in histogram: {interval} pm {errorMax}')
-	else: interval=0
-	return interval,yTrainSHist,yTrainBHist,yTestSHist,yTestBHist
 
 if plotPrediction_KS:
 # Plot the result: y_pred
